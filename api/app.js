a516381452b7d3aed7af0b883e1c5a80aef2331e
ilafper <ivanaalfaya@gmail.com>
2026-06-04 20:03:51 +0200

    todos jugadores

--- api/app.js
+++ api/app.js
@@ -103,29 +103,42 @@ app.get("/api/random", async (req, res) => {
 
 
 app.get("/api/allplayers", async (req, res) => {
-  //1. obtener todas las selecciones
-  const url =
-    "https://www.thesportsdb.com/api/v1/json/123/search_all_teams.php?l=English%20Premier%20League";
-  const rest = await fetch(url);
-  const data = await rest.json();
+  //1. obtener selecciones
+  
+  const url ="https://www.thesportsdb.com/api/v1/json/123/search_all_teams.php?l=FIFA%20World%20Cup";
+  const response = await fetch(url);
+  const seleciones_mundial = await response.json();
+  // eequipos del mundial
+  let equipos = seleciones_mundial.teams;
+  
+  // lista vacia donde iran los jugadores
+  let todos_jugadores = [];
+  //console.log(seleccionados);
+  
+  
+  for (let cada_equipo of equipos) {
+    const jugadoresRes = await fetch(`https://www.thesportsdb.com/api/v1/json/123/lookup_all_players.php?id=${cada_equipo.idTeam}`);
 
-  let totalJugadores = 0;
-
-  for (let cada_equipo of data.teams) {
-    const respuesta = await fetch(
-      `https://www.thesportsdb.com/api/v1/json/123/lookup_all_players.php?id=${cada_equipo.idTeam}`,
-    );
-
-    const cada_juagador = await respuesta.json();
-
-    if (cada_juagador.player) {
-      totalJugadores += cada_juagador.player.length;
+    //https://www.thesportsdb.com/api/v1/json/123/lookup_all_players.php?id=133604
+    const jugadoresData = await jugadoresRes.json();
+    if (jugadoresData.player) {
+      const jugadores_equipos= jugadoresData.player;
+      for (let cada_jugador of jugadores_equipos) {
+       todos_jugadores.push({
+        nombre_jugador:cada_jugador.strPlayer,
+        logo_seleccion:cada_equipo.strLogo,
+        imagen_player:cada_jugador.strCutout
+       });
+        
+      }
     }
+
+    
+    
   }
+  //console.log(jugadores_aleatorios);
 
-  console.log("Total jugadores:", totalJugadores);
-
-  res.json({ success: true, mensaje: "cromos aleatorios", totalJugadores });
+  res.json({ success: true, mensaje: "cromos aleatorios", todos_jugadores });
 });
 
 
@@ -281,237 +294,8 @@ app.post("/api/registro", async (req, res) => {
 
 //eliminar producto por id
 
-app.delete("/api/eliminarcliente/:id", async (req, res) => {
-  const { clientes } = await connectToMongoDB();
 
-  try {
-    const id_eliminar = req.params.id;
 
-    console.log(id_eliminar);
 
-    // borrar el borrar y su e vento
-    const resultado = await clientes.deleteOne({ code_user: id_eliminar });
-
-    if (resultado.deletedCount === 0) {
-      return res
-        .status(404)
-        .json({ success: false, error: "No se encontro el cliente" });
-    }
-
-    res.json({
-      success: true,
-      mensaje: "cliente con id: " + id_eliminar + "eliminado correctamente",
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Error al eliminar", detalle: error.message });
-  }
-});
-
-app.get("/api/filtronombre/:nombreBusqueda", async (req, res) => {
-  const { clientes } = await connectToMongoDB();
-
-  try {
-    const nombreBusqueda = req.params.nombreBusqueda;
-    console.log("Buscando nombre:", nombreBusqueda);
-
-    if (!nombreBusqueda) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Debes enviar un nombre" });
-    }
-
-    // busqueda parcial del campo nombre
-    const filtroNombre = await clientes
-      .find({ nombre: new RegExp(nombreBusqueda, "i") })
-      .toArray();
-    console.log(filtroNombre);
-
-    if (filtroNombre.length === 0) {
-      console.log("no hay coincidencias");
-      return res.json({ success: false, error: "no hay coincidencias" });
-    }
-
-    res.json({
-      success: true,
-      mensaje: "todas las coincidencias",
-      datos: filtroNombre,
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .json({
-        success: false,
-        error: "Error al encontrar",
-        detalle: error.message,
-      });
-  }
-});
-
-//filtro apellidos
-
-app.get("/api/filtroapellidos/:apeBusqueda", async (req, res) => {
-  const { clientes } = await connectToMongoDB();
-
-  try {
-    const apeBusqueda = req.params.apeBusqueda;
-    console.log("Buscando apellido:", apeBusqueda);
-
-    if (!apeBusqueda) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Debes enviar un apellido" });
-    }
-
-    // busqueda parcial del campo nombre
-    const filtroapellidos = await clientes
-      .find({ apellidos: new RegExp(apeBusqueda, "i") })
-      .toArray();
-    console.log(filtroapellidos);
-
-    if (filtroapellidos.length === 0) {
-      console.log("no hay coincidencias");
-      return res.json({ success: false, error: "no hay coincidencias" });
-    }
-
-    res.json({
-      success: true,
-      mensaje: "todas las coincidencias",
-      datos: filtroapellidos,
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .json({
-        success: false,
-        error: "Error al encontrar",
-        detalle: error.message,
-      });
-  }
-});
-
-app.get("/api/filtrotelefono/:tlfBusqueda", async (req, res) => {
-  const { clientes } = await connectToMongoDB();
-
-  try {
-    const tlfBusqueda = req.params.tlfBusqueda;
-    console.log("Buscando apellido:", tlfBusqueda);
-
-    if (!tlfBusqueda) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Debes enviar un apellido" });
-    }
-
-    // busqueda parcial del campo nombre
-    const filtrotelefono = await clientes
-      .find({ telefono: new RegExp(tlfBusqueda, "i") })
-      .toArray();
-
-    console.log(filtrotelefono);
-
-    if (filtrotelefono.length === 0) {
-      console.log("no hay coincidencias");
-      return res.json({ success: false, error: "no hay coincidencias" });
-    }
-
-    res.json({
-      success: true,
-      mensaje: "resultado filtro: ",
-      datos: filtrotelefono,
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .json({
-        success: false,
-        error: "Error al encontrar",
-        detalle: error.message,
-      });
-  }
-});
-
-app.get("/api/filtrocorreo/:correoBusqueda", async (req, res) => {
-  const { clientes } = await connectToMongoDB();
-
-  try {
-    const correoBusqueda = req.params.correoBusqueda;
-    console.log("Buscando correo:", correoBusqueda);
-
-    if (!correoBusqueda) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Debes enviar un apellido" });
-    }
-
-    // busqueda parcial del campo correo
-    const filtrocorreo = await clientes
-      .find({ correo: new RegExp(correoBusqueda, "i") })
-      .toArray();
-
-    console.log(filtrocorreo);
-
-    if (filtrocorreo.length === 0) {
-      console.log("no hay coincidencias");
-      return res.json({ success: false, error: "no hay coincidencias" });
-    }
-
-    res.json({
-      success: true,
-      mensaje: "todas las coincidencias",
-      datos: filtrocorreo,
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .json({
-        success: false,
-        error: "Error al encontrar",
-        detalle: error.message,
-      });
-  }
-});
-
-app.get("/api/filtrodireccion/:direcionBusqueda", async (req, res) => {
-  const { clientes } = await connectToMongoDB();
-
-  try {
-    const filtroDirec = req.params.direcionBusqueda;
-    console.log("Buscando correo:", filtroDirec);
-
-    if (!filtroDirec) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Debes enviar un dato" });
-    }
-
-    // busqueda parcial del campo correo
-    const filtrodireccion = await clientes
-      .find({ direccion: new RegExp(filtroDirec, "i") })
-      .toArray();
-
-    console.log(filtrodireccion);
-
-    if (filtrodireccion.length === 0) {
-      console.log("no hay coincidencias");
-      return res.json({ success: false, error: "no hay coincidencias" });
-    }
-
-    res.json({
-      success: true,
-      mensaje: "todas las coincidencias",
-      datos: filtrodireccion,
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .json({
-        success: false,
-        error: "Error al encontrar",
-        detalle: error.message,
-      });
-  }
-});
 
 module.exports = app;
